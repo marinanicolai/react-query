700740eedd1c3452bca631ec3d5f526a73fa793c
marina <nicolaimarina@gmail.com>
2022-11-15 08:41:54 -0500

    last updates

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import { useQuery } from "react-query";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home";
+import { CoinPage } from "./pages/CoinPage";
 
 // async function fetchPosts() {
 //   const { data } = await axios.get('https://jsonplaceholder.typicode.com/posts') return data }
 
 function App() {
-  const client = new QueryClient();
+  const [client] = React.useState(() => new QueryClient());
 
   return (
     <>
@@ -16,6 +17,7 @@ function App() {
         <Router>
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/coin" element={<CoinPage />} />
           </Routes>
         </Router>
       </QueryClientProvider>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,35 @@
 import { isError, useQuery } from "@tanstack/react-query";
 import axios from "axios";
+import { useState } from "react";
+import { useAsyncMutation } from "../hooks/useAsyncMutation";
+import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export const Home = () => {
-  const { data, isLoading, isError } = useQuery(["id"], () => {
+  // const [coin, setCoin] = useState("");
+  const { coin } = useParams();
+
+  const { data, isLoading, isError } = useQuery(["id", coin], () => {
     return axios
       .get(
         "https://api.coingecko.com/api/v3/coins/bitcoin?tickers=true&market_data=true&community_data=true&developer_data=true&sparkline=true"
       )
       .then((res) => res.data);
+  });
+  //   const fetchTodos = (id) => {
+  //     return axios
+  //       .get(
+  //         "https://api.coingecko.com/api/v3/coins/${id}?tickers=true&market_data=true&community_data=true&developer_data=true&sparkline=true"
+  //       )
+  //       .then((res) => res.data);
+  //   };
+
+  //   const query = useQuery({ queryKey: ["todos"], queryFn: fetchTodos });
+
+  console.log(data);
+
+  const plainMutate = useAsyncMutation(() => {
+    alert("clicked");
   });
 
   if (isError) {
@@ -21,6 +43,13 @@ export const Home = () => {
     <>
       <h1>this is Home page</h1>
       <p>{data?.name}</p>
+      <form>
+        <button onClick={() => plainMutate()}>Run plain Mutation</button>
+        <input placeholder="Search the coin" ></input>
+        <Link to="/coin">
+          <button>Update the coin</button>
+        </Link>
+      </form>
     </>
   );
 };
